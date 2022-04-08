4eee7ad2213772c44d4b93a89cc1dc1e71c0c241
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2022-04-08 15:33:49 +0530

    Update Merge.c

--- DAA/Merge.c
+++ DAA/Merge.c
@@ -1,72 +1,65 @@
-#include<stdio.h> 
-void merg(int a[],int l,int h,int m,int *c)
+class Merge
 {
-	int i=0,j=m+1,k=0;
-	int b[h-l+1];
-	while(i<=m&&j<=h)
+	void merg(int a[],int l,int h,int m,int n)
 	{
-		if(a[i]<a[j])
+		int i=0,j=m+1,k=0;
+		int b[]=new int[h+l+1];
+		while(i<=m&&j<=h)
 		{
-		   b[k]=a[i];
-		   k++;
-		   i++;
+			if(a[i]<=a[j])
+			{
+		   		b[k]=a[i];
+		   		k++;
+		   		i++;
+			}
+			else
+			{
+                b[k]=a[j];
+		   		k++;
+		   		j++;
+			} 
+                 
+        }
+		if(i>m)
+		{
+			for(;j<=h;j++)
+			{
+				b[k]=a[j];
+				k++;
+			}
 		}
 		else
 		{
-                   b[k]=a[j];
-		   k++;
-		   j++;
-		}  
-                (*c)++; 
-                 
-         }
-		for(;j<=h;j++)
-		{
-			b[k]=a[j];
-			k++;
+			for(;i<=m;i++)
+			{
+				b[k]=a[i];
+				k++;
+			}
 		}
-		for(;i<=m;i++)
+ 		for(k=l;k<=h;k++)
 		{
-			b[k]=a[i];
-			k++;
+            a[k]=b[k];
 		}
-	int o=0;
- 	for(int f=l;f<=h;f++)
+	} 
+	void MergeS(int a[],int l,int h,int n)
 	{
-            a[f]=b[o];
-	    o++;
+		if(l<h)
+		{ 
+			int m=(l+h)/2;
+			MergeS(a, l, m,n);
+			MergeS(a,m+1,h,n);
+			merg(a,l,h,m,n);
+		}
+	} 
+	public static void main(String args[])
+	{
+		int a[]={2,0,5,3,7,8};
+		Merge m= new Merge(); 
+		int n=a.length;
+		m.MergeS(a,0,n-1,n);
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(a[i]+" ");
+		} 
 	}
 }
-			
-void mergS(int a[],int l,int h,int *c)
-{
-	if(l<h)
-	{
-		int m=l+(h-l)/2;
-		mergS(a,l,m,c);
-		mergS(a,m+1,h,c);
-		merg(a,l,h,m,c);
-	}
-}
-
-int main()
-{     
-        int n,c=0;
-	printf("Enter the Number of elements: ");
-	scanf("%d",&n);
-	int a[n];
-	
-	printf("Enter the elements: \n");
-	for(int i=0;i<n;i++)
-	  scanf("%d",&a[i]); 
-	
-	mergS(a,0,n-1,&c); 
-	printf("The Sorted Array:\n");
-	for(int i=0;i<n;i++)
-	printf("%d\n",a[i]);
-
-	printf("\nNo. of Comparisons: %d",c); 
-	return 0;
-}
-
-	
