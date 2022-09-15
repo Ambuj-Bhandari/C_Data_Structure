808474239d82f6a92fe6d8f24dbed8d4c7825fb7
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2022-09-15 08:43:02 +0530

    Add files via upload

---
+++ CBNST/Ambuj/4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+	int x=fork();
+	if(x!=0)
+	{
+		printf("Process ID of parent: %d\n",getpid());
+	}
+	else
+	{
+		sleep(5);
+		printf("Process ID Child:%d \n",getpid());
+		printf("Its Parent ID: %d\n",getppid());
+	}
+	return 0;
+}
