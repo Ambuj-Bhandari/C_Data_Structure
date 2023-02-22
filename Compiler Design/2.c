8ad12633ebbd8aef3f8517344c5829369dd90bd6
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2023-02-22 09:27:06 +0530

    Update 2.c

--- Compiler Design/2.c
+++ Compiler Design/2.c
@@ -5,8 +5,8 @@
 
 DIGIT [0=9]
 %%
-    {DIGIT}* {ECHO; printf("Integer\n");}  //ECHO prints the currently enetered number
-    {DIGIT}* ?\{DIGIT}* {ECHO; printf("Float\n");} /* ?\ means that the left hand side may or maynot be present*/
+    {DIGIT}* {ECHO;printf("Integer\n");}  //ECHO prints the currently enetered number
+    {DIGIT}* ?\{DIGIT}* {ECHO;printf("Float\n");} /* ?\ means that the left hand side may or maynot be present*/
 %%
 
 yywrap(){}
